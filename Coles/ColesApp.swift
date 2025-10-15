75fdc1c7ec5b2d5f90e6c110b781e2a557b0cd93
Tim Kreger <tim.kreger@empoer.me>
2025-10-16 09:57:45 +1100

    Initial Commit

---
+++ Coles/ColesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColesApp.swift
+//  Coles
+//
+//  Created by Tim Kreger on 16/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct ColesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ColesTests/ColesTests.swift
@@ -0,0 +1,17 @@
+//
+//  ColesTests.swift
+//  ColesTests
+//
+//  Created by Tim Kreger on 16/10/2025.
+//
+
+import Testing
+@testable import Coles
+
+struct ColesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
